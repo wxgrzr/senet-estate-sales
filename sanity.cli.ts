560eba1b624f8d2da45047488c1c863c499e9bc9
William Greer <willfromspace@gmail.com>
2025-05-02 17:21:34 -0700

    sanity config

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,16 +1,21 @@
 import { defineCliConfig } from 'sanity/cli';
 
+const projectId = process.env.SANITY_STUDIO_PROJECT_ID || '<your project ID>';
+const dataset = process.env.SANITY_STUDIO_DATASET || 'production';
+
 export default defineCliConfig({
   api: {
-    projectId: 'lc0v2d89',
-    dataset: 'production',
+    projectId,
+    dataset,
   },
   project: {
     basePath: '/studio',
   },
+  studioHost: process.env.SANITY_STUDIO_STUDIO_HOST || '', // Visit https://www.sanity.io/docs/environment-variables to learn more about using environment variables for local & production.
+
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
-  autoUpdates: true,
+  autoUpdates: false,
 });
