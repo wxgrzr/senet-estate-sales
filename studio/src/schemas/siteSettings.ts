9a9d4f72b3f207091d61ebfd67a5b584cd36dc39
William Greer <willfromspace@gmail.com>
2025-05-16 16:07:51 -0700

    feat(comps): fix carousels, new image assets, dynamic content for contact info

--- studio/src/schemas/siteSettings.ts
+++ studio/src/schemas/siteSettings.ts
@@ -2,50 +2,21 @@ import { defineField, defineType } from 'sanity';
 import { CogIcon } from '@sanity/icons';
 
 export default defineType({
-  name: 'siteSettings',
+  name: 'settings',
   title: 'Site Settings',
   icon: CogIcon,
   type: 'document',
   fields: [
     defineField({
-      name: 'phoneNumber',
-      title: 'Phone number',
+      name: 'title',
+      title: 'Site Title',
       type: 'string',
     }),
     defineField({
-      name: 'businessAddress',
-      title: 'Business address',
-      type: 'object',
-      fields: [
-        {
-          name: 'addressLine1',
-          type: 'string',
-          title: 'Address line 1',
-          description: 'Street number/name',
-        },
-        {
-          name: 'addressLine2',
-          type: 'string',
-          title: 'Address line 2',
-          description: `Format: "City, State ZIP"`,
-        },
-      ],
+      name: 'description',
+      title: 'Site Description',
+      type: 'text',
     }),
-    defineField({
-      name: 'emailAddress',
-      type: 'string',
-      title: 'Email address',
-    }),
-    // defineField({
-    //   name: 'title',
-    //   title: 'Site Title',
-    //   type: 'string',
-    // }),
-    // defineField({
-    //   name: 'description',
-    //   title: 'Site Description',
-    //   type: 'text',
-    // }),
     // TODO: Add more (e.g., social links, logo, etc.)
   ],
 });
