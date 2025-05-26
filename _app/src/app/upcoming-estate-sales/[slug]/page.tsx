bbfd3e928850e99333bf17e0f4d6e6beaabef58f
William Greer <willfromspace@gmail.com>
2025-05-26 11:22:38 -0700

    feat(pages): layout for estate sales page

--- _app/src/app/upcoming-estate-sales/[slug]/page.tsx
+++ _app/src/app/upcoming-estate-sales/[slug]/page.tsx
@@ -52,74 +52,80 @@ export default async function Page({
 
   const address = post?.location?.fullAddress;
   const eventDates = post?.eventDates || [];
+  console.log(address);
 
   return (
-    <Container>
-      <section className='relative flex flex-col py-12 md:py-8'>
-        {/* Breadcrumb links */}
-        <div className='mb-6 hidden md:block'>
-          <Breadcrumbs
-            items={[
-              { label: 'Home', href: '/' },
-              {
-                label: 'Upcoming Estate Sales',
-                href: '/upcoming-estate-sales',
-              },
-              { label: post?.title || 'Estate Sale' },
-            ]}
-          />
-        </div>
-        <div className='mb-8 md:hidden'>
-          <LinkButton href='/upcoming-estate-sales' variant='text'>
-            ← Back to Upcoming Estate Sales
-          </LinkButton>
-        </div>
-        {/* Grid Layout */}
-        <div className='px-4'>
-          <div className='grid gap-x-4 gap-y-8 md:grid-cols-2 md:grid-rows-2'>
-            <div id='event-details' className='flex flex-1 flex-col'>
-              <h1
-                id='event-title'
-                className='mb-8 text-4xl font-bold tracking-tighter text-pretty md:text-5xl lg:text-6xl'
-              >
-                {post.title}
-              </h1>
-              <div id='event-dates' className='mb-8'>
-                <h2 className='mb-2 text-2xl tracking-tighter'>
-                  <b>Event dates</b>
-                </h2>
-                <div className='text-lg text-gray-600'>
-                  {eventDates.map((date: string, index: number) => (
+    <div>
+      {/* Breadcrumb links */}
+      <div className='mb-12 hidden md:block'>
+        <Breadcrumbs
+          items={[
+            { label: 'Home', href: '/' },
+            {
+              label: 'Upcoming Estate Sales',
+              href: '/upcoming-estate-sales',
+            },
+            { label: post?.title || 'Estate Sale' },
+          ]}
+        />
+      </div>
+      <div className='mb-8 md:hidden'>
+        <LinkButton href='/upcoming-estate-sales' variant='text'>
+          ← Back to Upcoming Estate Sales
+        </LinkButton>
+      </div>
+      <section className='px-4 pb-16'>
+        <div className='grid gap-x-4 gap-y-8 md:grid-cols-2 md:grid-rows-2'>
+          <div id='event-details' className='flex flex-1 flex-col'>
+            <h1
+              id='event-title'
+              className='mb-8 text-4xl font-bold tracking-tighter text-pretty md:text-5xl lg:text-6xl'
+            >
+              {post.title}
+            </h1>
+            <div id='event-dates' className='mb-8'>
+              <h2 className='mb-2 text-2xl tracking-tighter'>
+                <b>Event dates</b>
+              </h2>
+              <div className='text-lg text-gray-600'>
+                {eventDates.length > 0 ? (
+                  eventDates.map((date: string, index: number) => (
                     <p key={index}>
                       <DateFormatter dateString={date} time />
                     </p>
-                  ))}
-                </div>
+                  ))
+                ) : (
+                  <span className='tracking-tighter italic'>TBD</span>
+                )}
               </div>
-              <div id='event-location'>
-                <h2 className='mb-2 text-2xl tracking-tighter'>
-                  <b>Google maps link</b>
-                </h2>
+            </div>
+            <div id='event-location'>
+              <h2 className='mb-2 text-2xl tracking-tighter'>
+                <b>Google maps link</b>
+              </h2>
+              {address.length > 0 ? (
                 <OpenInMapsButton
                   address={address}
                   className="text-lg before:content-['📍']"
                 />
-              </div>
+              ) : (
+                <span className='tracking-tighter italic'>TBD</span>
+              )}
             </div>
-            {/* Row 1 Column 2: Cover Image */}
+          </div>
+          <div className='relative flex min-h-72 w-full shrink-0 items-center justify-center'>
             {coverImage && (
-              <div className='relative flex min-h-72 w-full shrink-0 items-center justify-center'>
-                <Image
-                  src={coverImage}
-                  alt={post?.title}
-                  className='rounded-xl object-cover'
-                  fill
-                  priority
-                />
-              </div>
+              <Image
+                src={coverImage}
+                alt={post?.title}
+                className='rounded-xl object-cover'
+                fill
+                priority
+              />
             )}
-            {/* Row 2 Column 1: Description (PortableText) */}
-            <div>
+          </div>
+          <div>
+            {post.body ? (
               <div className='prose max-h-96 min-h-50 overflow-y-auto rounded-lg bg-gray-50 px-4 py-2 shadow-inner'>
                 <p className='mb-2 text-sm text-gray-400'>
                   Updated: {new Date(post?._updatedAt).toLocaleDateString()}
@@ -128,14 +134,16 @@ export default async function Page({
                   <PortableText value={post.body} />
                 )}
               </div>
-            </div>
-            {/* Row 2 Column 2: Gallery */}
-            <div className='max-h-96 min-h-50 overflow-y-auto rounded-lg bg-gray-50 p-4 shadow-inner'>
-              {gallery?.length > 0 && <Gallery images={images} />}
-            </div>
+            ) : (
+              ''
+            )}
+          </div>
+          {/* Row 2 Column 2: Gallery */}
+          <div className='max-h-96 min-h-50 overflow-y-auto rounded-lg bg-gray-50 p-4 shadow-inner'>
+            {gallery?.length > 0 && <Gallery images={images} />}
           </div>
         </div>
       </section>
-    </Container>
+    </div>
   );
 }
