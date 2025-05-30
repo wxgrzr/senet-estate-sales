1ee0620ab275428fe6a45d2b2665385459b9e425
William Greer <willfromspace@gmail.com>
2025-05-29 17:37:14 -0700

    feat(comps): generate reviews types for new schema

--- _app/src/lib/constants.ts
+++ _app/src/lib/constants.ts
@@ -1,11 +1,11 @@
-export const HOME_OG_IMAGE_URL =
-  'https://og-image.vercel.app/Next.js%20Blog%20Starter%20Example.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg';
-
 export const PAGES = [
   { label: 'Home', href: '/' },
   { label: 'Estate Sales', href: '/upcoming-estate-sales' },
-  { label: 'FAQs', href: '/faq' },
-  { label: 'Schedule a Consultation', href: '/schedule-a-consultation' },
+  { label: 'FAQs', href: '/estate-sale-questions' },
+  {
+    label: 'Schedule a Consultation',
+    href: '/request-estate-sale-consultation',
+  },
 ];
 
 export const TESTIMONIALS = [
